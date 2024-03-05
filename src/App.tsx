726ed76c4a9784e4db9bf90fe2a2e1f735472860
Your Name <you@example.com>
2024-03-05 14:14:56 +0200

    new packege

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Input from './components/UI/Input/Input'
 import './styles/modules/global.scss'
 
 function App() {
+	
 	return (
 		<>
-			<Input placeholder='Search' variant='default' />
+
 		</>
 	)
 }
